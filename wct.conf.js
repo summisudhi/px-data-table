233a4242c0ee9cfd5c07ced7b1d2a4d3e7380574
Sirisha Aluri <sirisha.aluri@ge.com>
2016-03-16 15:03:44 -0700

    Added further more browsers

--- wct.conf.js
+++ wct.conf.js
@@ -27,6 +27,16 @@ module.exports = {
           "platform": "Windows 8.1",
           "version": "11"
         },
+        {
+          "browserName": "internet explorer",
+          "platform": "Windows 10",
+          "version": "11"
+        },
+        {
+          "browserName": "internet explorer",
+          "platform": "Windows 8",
+          "version": "11"
+        },
          {
           "browserName": "firefox",
           "platform": "Windows 8.1",
@@ -37,7 +47,11 @@ module.exports = {
           "platform": "Windows 8.1",
           "version": "48"
         },
-        
+         {
+          "browserName": "safari",
+          "platform": "OS X 10.10",
+          "version": "8"
+        },
          
         
          {
